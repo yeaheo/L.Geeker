a2761a25eeadd81f0d9832a89ca9db8dc2d526aa
Yeah.Bravely <xiaoteng2767@vip.qq.com>
2017-11-02 11:29:04 +0800

    Update shadowsocksR.sh

--- Shell/shadowsocksR.sh
+++ Shell/shadowsocksR.sh
@@ -4,7 +4,7 @@ export PATH
 #=================================================================#
 #   System Required:  CentOS 6,7, Debian, Ubuntu                  #
 #   Description: One click Install ShadowsocksR Server            #
-#   Author: Teddysun <i@teddysun.com>                             #
+#   Author: Lv Xiaoteng <youger.lv@gmail.com>                             #
 #   Thanks: @breakwa11 <https://twitter.com/breakwa11>            #
 #   Intro:  https://shadowsocks.be/9.html                         #
 #=================================================================#
